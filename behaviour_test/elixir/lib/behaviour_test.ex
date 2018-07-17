5a0f99c5728e2eb6e6102eff6e4bcd3298639c03
Pedro Mangabeira <pedro@getonce.com>
2018-07-17 12:00:23 +0100

    Remove useless file

--- behaviour_test/elixir/lib/behaviour_test.ex
+++ behaviour_test/elixir/lib/behaviour_test.ex
@@ -1,11 +1,17 @@
 defmodule BehaviourTest do
   use Application
   require Logger
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
+
     children = [
       worker(Clusterable, [], restart: :transient),
-      Plug.Adapters.Cowboy2.child_spec(scheme: :http, plug: BehaviourTest.Route, options: [port: 8080])
+      Plug.Adapters.Cowboy2.child_spec(
+        scheme: :http,
+        plug: BehaviourTest.Route,
+        options: [port: 8080]
+      )
     ]
 
     Logger.info("Started application")
